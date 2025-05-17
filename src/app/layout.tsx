4a6d9ddbcf60c31f3dfaab585562a50117d2f3ad
André Melo <andregabrielmelo@gmail.com>
2025-05-17 16:13:37 -0300

    Adiciona configurações de tema (modo claro e escuro) para a aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/themes/theme-provider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,11 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark overscroll-contain scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="md:px-32 px-8">
+
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
